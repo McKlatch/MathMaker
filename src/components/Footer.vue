f39962dbfaada954561025be54ef69108133fe1a
McKlatch <david@hawkandlime.com>
2020-11-20 23:01:57 +0000

    Update link [end of 8bitephod url]

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-muted text-center">Made by <a href="http://twitter.com/mcklatch">David McClatchey</a></p>
+  <p class="text-muted text-center">Made by <a href="https://mcklatch.com/hi" rel="noreferrer" target="_blank">McKlatch</a> - see more at <a href="https://dev.mcklatch.com" rel="noreferrer" target="_blank">dev.mcklatch.com</a></p>
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,9 +10,9 @@
     </template>
     <hr>
     <p>
-      Entire project made personally with all features designed and implemented by <a href="http://twitter.com/mcklatch" rel="noopener noreferrer" target="_blank">David McClatchey</a>.
+      Entire project made personally with all features designed and implemented by <a href="https://mcklatch.com/hi" rel="noopener noreferrer" target="_blank">David McClatchey</a>.
       <br>
-      Made to the specifications of the final project assignment at <a href="https://cs50.harvard.edu/" rel="noopener noreferrer" target="_blank">Harvard CS50</a> 2016.
+      Made to the specifications of the final project assignment at <a href="http://cs50.tv/2016/fall/" rel="noopener noreferrer" target="_blank">Harvard CS50</a> 2016.
     </p>
     <b-btn variant="primary" to="/form">Get Started</b-btn>
   </b-jumbotron>
